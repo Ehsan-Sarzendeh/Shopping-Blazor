6ad9092429edf21fa4b66385a0f1a82d190f0056
Ehsan Sarzendeh <ehsansarzendeh@gmail.com>
2024-11-28 22:48:02 +0330

    some chnages

---
+++ ShoppingBlazor/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingBlazor.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Group { get; set; }
+}
---
+++ ShoppingBlazor/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingBlazor.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "نام کالا الزامی است")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "قیمت الزامی است")]
+    [Range(0, int.MaxValue, ErrorMessage = "قیمت باید عددی مثبت باشد")]
+    public decimal Price { get; set; }
+
+    [Required(ErrorMessage = "موجودی الزامی است")]
+    [Range(0, int.MaxValue, ErrorMessage = "موجودی باید عددی مثبت باشد")]
+    public int Availability { get; set; }
+
+    public string? SupplierName { get; set; }
+    public string? SupplierEmail { get; set; }
+
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+
+    public string? ImagePath { get; set; }
+    public string? CatalogPath { get; set; }
+
+    public List<string> Colors { get; set; } = new();
+    public string? Description { get; set; }
+
+
+    [Required(ErrorMessage = "دسته‌بندی الزامی است")]
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+
+    [Required(ErrorMessage = "برند الزامی است")]
+    public int BrandId { get; set; }
+    public Brand Brand { get; set; }
+}
